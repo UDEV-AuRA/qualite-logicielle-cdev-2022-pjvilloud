55277c86921bc4d4e12930414d96e73830f6772d
Pierre-Julien VILLOUD <pjvilloud@protonmail.com>
2022-04-25 15:10:52 +0200

    Add Unit tests for Employe.getNombreAnneeAnciennete

---
+++ src/test/java/com/ipiecoles/java/java350/model/EmployeTest.java
@@ -0,0 +1,63 @@
+package com.ipiecoles.java.java350.model;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+
+public class EmployeTest {
+
+    @Test
+    public void testGetNbAnneesAncienneteDateEmbaucheNow(){
+        //Given
+        Employe employe = new Employe();
+        employe.setDateEmbauche(LocalDate.now());
+
+        //When
+        Integer nbAnneesAnciennete = employe.getNombreAnneeAnciennete();
+
+        //Then
+        Assertions.assertThat(nbAnneesAnciennete).isZero();
+
+    }
+
+    @Test
+    public void testGetNbAnneesAncienneteDateEmbauchePassee(){
+        //Given
+        Employe employe = new Employe();
+        employe.setDateEmbauche(LocalDate.now().minusYears(2));
+
+        //When
+        Integer nbAnneesAnciennete = employe.getNombreAnneeAnciennete();
+
+        //Then
+        Assertions.assertThat(nbAnneesAnciennete).isEqualTo(2);
+    }
+
+    @Test
+    public void testGetNbAnneesAncienneteDateEmbaucheFuture(){
+        //Given
+        Employe employe = new Employe();
+        employe.setDateEmbauche(LocalDate.now().plusYears(2));
+
+        //When
+        Integer nbAnneesAnciennete = employe.getNombreAnneeAnciennete();
+
+        //Then
+        Assertions.assertThat(nbAnneesAnciennete).isZero();
+    }
+
+    @Test
+    public void testGetNbAnneesAncienneteDateEmbaucheNull(){
+        //Given
+        Employe employe = new Employe();
+        employe.setDateEmbauche(null);
+
+        //When
+        Integer nbAnneesAnciennete = employe.getNombreAnneeAnciennete();
+
+        //Then
+        Assertions.assertThat(nbAnneesAnciennete).isZero();
+    }
+
+}
